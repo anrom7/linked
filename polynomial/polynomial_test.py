ca2249efc56067b2d2bab2f2e7bf0b4d130e14ed
oles <anrom7@gmail.com>
2020-04-28 20:51:18 +0300

    rename module

---
+++ polynomial/polynomial_test.py
@@ -0,0 +1,34 @@
+from polynomial import Polynomial
+
+poly1 = Polynomial()
+poly2 = Polynomial()
+
+data_file = open("data1.txt", "r")
+data_list = data_file.readlines()
+data_file.close()
+for line in data_list:
+    degree, coefficient = line.strip().split()
+    poly1._appendTerm(float(degree), float(coefficient))
+
+data_file = open("data2.txt", "r")
+data_list = data_file.readlines()
+data_file.close()
+for line in data_list:
+    degree, coefficient = line.strip().split()
+    poly2._appendTerm(float(degree), float(coefficient))
+
+#print(poly1)
+#print(poly2)
+
+
+print(poly1.degree())
+print(poly2.degree())
+
+print(poly1[2.0])
+
+new_p = poly1 + poly2
+print(new_p[3.0], new_p[2.0],new_p[1.0],new_p[0.0])
+
+print(poly1 + poly2)
+print(poly1 - poly2)
+print(poly1 * poly2)
