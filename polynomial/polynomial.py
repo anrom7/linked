d510123fe96211405eb8128211ddbbf5f3e6557a
anrom7 <anrom7@gmail.com>
2020-04-29 12:53:17 +0300

    Update polynomial.py
    del methods

--- polynomial/polynomial.py
+++ polynomial/polynomial.py
@@ -39,7 +39,7 @@ class Polynomial :
         return result
 
     # Polynomial addition: newPoly = self + rhsPoly.
-    #def __add__(self, rhsPoly):
+    def __add__(self, rhsPoly):
     #    pass
 
     # Polynomial subtraction: newPoly = self - rhsPoly.
@@ -64,83 +64,7 @@ class Polynomial :
             i += 1
         return newPoly
 
-    # Helper method for appending terms to the polynomial.
-    def _appendTerm(self, degree, coefficient) :
-        if coefficient != 0.0 :
-            newTerm = _PolyTermNode(degree, coefficient)
-            if self._polyHead is None :
-                self._polyHead = newTerm
-            else :
-                self._polyTail.next = newTerm
-            self._polyTail = newTerm
 
-    def __add__(self, rhsPoly):
-        assert self.degree() >= 0 and rhsPoly.degree() >= 0, "Addition only allowed on non -empty polynomials."
-        newPoly = Polynomial()
-        nodeA = self._polyHead
-        nodeB = rhsPoly._polyHead
-
-        # Add corresponding terms until one list is empty.
-        while nodeA is not None and nodeB is not None:
-            if nodeA.degree > nodeB.degree:
-                degree = nodeA.degree
-                value = nodeA.coefficient
-                nodeA = nodeA.next
-            elif nodeA.degree < nodeB.degree:
-                degree = nodeB.degree
-                value = nodeB.coefficient
-                nodeB = nodeB.next
-            else:
-                degree = nodeA.degree
-                value = nodeA.coefficient + nodeB.coefficient
-                nodeA = nodeA.next
-                nodeB = nodeB.next
-            newPoly._appendTerm(degree, value)
-
-        # If self list contains more terms append them.
-        while nodeA is not None:
-            newPoly._appendTerm(nodeA.degree, nodeA.coefficient)
-            nodeA = nodeA.next
-
-        # Or if rhs contains more terms append them.
-        while nodeB is not None:
-            newPoly._appendTerm(nodeB.degree, nodeB.coefficient)
-            nodeB = nodeB.next
-
-        return newPoly
-
-    def __mul__(self, rhsPoly):
-        assert self.degree() >= 0 and rhsPoly.degree() >= 0, "Multiplication only allowed on non -empty polynomials."
-
-        # Create a new polynomial by multiplying rhsPoly by the first term.
-        node = self._polyHead
-        newPoly = rhsPoly._termMultiply(node)
-
-        # Iterate through the remaining terms of the poly computing the
-        # product of the rhsPoly by each term.
-        node = node.next
-        while node is not None:
-            tempPoly = rhsPoly._termMultiply(node)
-            newPoly = newPoly + tempPoly
-            node = node.next
-        return newPoly
-
-    # Helper method for creating a new polynomial from multiplying an
-    # existing polynomial by another term.
-    def _termMultiply(self, termNode):
-        newPoly = Polynomial()
-        # Iterate through the terms and compute the product of each term and
-        # the term in termNode.
-        curr = self._polyHead
-        while curr is not None:
-            # Compute the product of the term.
-            newDegree = curr.degree + termNode.degree
-            newCoeff = curr.coefficient * termNode.coefficient
-            # Append it to the new polynomial.
-            newPoly._appendTerm(newDegree, newCoeff)
-            # Advance the current pointer.
-            curr = curr.next
-        return newPoly
 
     def __str__(self):
         pass
