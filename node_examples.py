fed96cb7fe87721b42f130d2ca3f24ea8c1fad84
anrom7 <a.romanyuk@ucu.edu.ua>
2021-04-12 12:51:46 +0300

    Update and rename node_test.py to node_examples.py
    change name

--- node_examples.py
+++ node_examples.py
@@ -1,5 +1,5 @@
 """
-File: test_node.py
+File: node_examples.py
 
 Tests the Node class.
 """
@@ -86,30 +86,3 @@ while probe is not  None:
     probe = probe.next
 
 print("______________________")
-
-
-class CircularLinkedList:
-    def __init__(self):
-        self.head = Node(None, None) # this is the sentinel node!
-        self.head.next = self.head   # link it to itself
-
-    def add(self, data):             # no special case code needed for an empty list
-        self.head.next = Node(data, self.head.next)
-
-    def __contains__(self, data):    # example algorithm, implements the "in" operator
-        current = self.head.next
-        while current != self.head:
-            if current.data == data: # element found
-                return True
-            current = current.next
-        return False
-        
-
-c = CircularLinkedList()
-for count in range(1, 6):
-    c.add(count)
-
-probe = c.head.next
-while probe is not c.head:
-    print(probe.data)
-    probe = probe.next
