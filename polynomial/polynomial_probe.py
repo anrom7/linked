cc3ef5ddd1e0a3da2ca5741e3f617e976e54da5d
anrom7 <a.romanyuk@ucu.edu.ua>
2021-04-12 12:21:08 +0300

    Update and rename polynomial_test.py to polynomial_probe.py
    change name

--- polynomial/polynomial_probe.py
+++ polynomial/polynomial_probe.py
@@ -8,14 +8,14 @@ data_list = data_file.readlines()
 data_file.close()
 for line in data_list:
     degree, coefficient = line.strip().split()
-    poly1._appendTerm(float(degree), float(coefficient))
+    poly1._append_term(float(degree), float(coefficient))
 
 data_file = open("data2.txt", "r")
 data_list = data_file.readlines()
 data_file.close()
 for line in data_list:
     degree, coefficient = line.strip().split()
-    poly2._appendTerm(float(degree), float(coefficient))
+    poly2._append_term(float(degree), float(coefficient))
 
 #print(poly1)
 #print(poly2)
