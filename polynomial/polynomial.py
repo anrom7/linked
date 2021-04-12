5cfcbc412cc16606d225a0d1d93da887ee2fadb2
anrom7 <a.romanyuk@ucu.edu.ua>
2021-04-12 12:19:30 +0300

    Update polynomial.py
    edit _

--- polynomial/polynomial.py
+++ polynomial/polynomial.py
@@ -4,71 +4,70 @@ class Polynomial :
     # Create a new polynomial object.
     def __init__(self, degree = None, coefficient = None):
         if degree is None :
-            self._polyHead = None
+            self._poly_head = None
         else :
-            self._polyHead = _PolyTermNode(degree, coefficient)
-        self._polyTail = self._polyHead
+            self._poly_head = _PolyTermNode(degree, coefficient)
+        self._poly_tail = self._poly_head
 
     # Return the degree of the polynomial.
     def degree(self):
-        if self._polyHead is None :
+        if self._poly_head is None :
             return -1
         else :
-            return self._polyHead.degree
+            return self._poly_head.degree
 
     # Return the coefficient for the term of the given degree.
     def __getitem__(self, degree):
         assert self.degree() >= 0, "Operation not permitted on an empty polynomial."
-        curNode = self._polyHead
-        while curNode is not None and curNode.degree >= degree :
-            curNode = curNode.next
+        cur_node = self._poly_head
+        while cur_node is not None and cur_node.degree >= degree :
+            cur_node = cur_node.next
 
-        if curNode is None or curNode.degree != degree :
+        if cur_node is None or cur_node.degree != degree :
             return 0.0
         else :
-            return curNode.coefficient
+            return cur_node.coefficient
 
     # Evaluate the polynomial at the given scalar value.
     def evaluate(self, scalar):
         assert self.degree() >= 0, "Only non -empty polynomials can be evaluated."
         result = 0.0
-        curNode = self._polyHead
-        while curNode is not None :
-            result += curNode.coefficient * (scalar ** curNode.degree)
-            curNode = curNode.next
+        cur_node = self._poly_head
+        while cur_node is not None :
+            result += cur_node.coefficient * (scalar ** cur_node.degree)
+            cur_node = cur_node.next
         return result
 
-    # Polynomial addition: newPoly = self + rhsPoly.
-    def __add__(self, rhsPoly):
-    #    pass
-
-    # Polynomial subtraction: newPoly = self - rhsPoly.
-    def __sub__(self, rhsPoly):
+    # Polynomial addition: newPoly = self + rhs_poly.
+    def __add__(self, rhs_poly):
         pass
 
-    # Polynomial multiplication: newPoly = self * rhsPoly.
-    #def __mul__(self, rhsPoly):
-    #    pass
+    # Polynomial subtraction: newPoly = self - rhs_poly.
+    def __sub__(self, rhs_poly):
+        pass
 
-    def simple_add(self, rhsPoly):
-        newPoly = Polynomial()
-        if self.degree() > rhsPoly.degree():
-            maxDegree = self.degree()
+    # Polynomial multiplication: newPoly = self * rhs_poly.
+    def __mul__(self, rhs_poly):
+        pass
+
+    def simple_add(self, rhs_poly):
+        new_poly = Polynomial()
+        if self.degree() > rhs_poly.degree():
+            max_degree = self.degree()
         else:
-            maxDegree = rhsPoly.degree()
+            max_megree = rhs_poly.degree()
 
-        i = maxDegree
+        i = max_degree
         while i >= 0:
-            value = self[i] + rhsPoly[i]
-            newPoly._appendTerm(i, value)
+            value = self[i] + rhs_poly[i]
+            new_poly._append_term(i, value)
             i += 1
-        return newPoly
-
-
+        return new_poly
 
     def __str__(self):
         pass
 
+    
 # Class for creating polynomial term nodes used with the linked list.
 class _PolyTermNode(object):
     def __init__(self, degree, coefficient):
